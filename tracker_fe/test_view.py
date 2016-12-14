09325d1f18941f2c6dbf9a4171a4b8215872e14d
Aboobacker <aboobacker@hamon.in>
2016-12-14 13:26:02 +0530

    fixed assertion error

--- tracker_fe/test_view.py
+++ tracker_fe/test_view.py
@@ -8,6 +8,6 @@ class indextest(TestCase):
     def test_index_view(self):
         #res=index(self)
         response = self.client.get(reverse('tracker_fe:index'))
-        self.assertEqual("Hello, world. You are at the tracking system" , response.content)
+        self.assertEqual("Hello, world. You\'re at the tracking system" , response.content)
     
 
