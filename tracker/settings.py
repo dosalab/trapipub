f43f197a141475b2d2c5b5015cbb82c4feafa5cd
Aboobacker <aboobacker@hamon.in>
2016-12-14 13:10:47 +0530

    configure app with project

--- tracker/settings.py
+++ tracker/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'tracker_fe.apps.TrackerFrontEnd',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
