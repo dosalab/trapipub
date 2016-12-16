fefbe346a790c517f08aad07769b32091f8e1182
Aboobacker <aboobacker@hamon.in>
2016-12-16 16:10:13 +0530

    Dummy message update

--- tracker_fe/views.py
+++ tracker_fe/views.py
@@ -4,7 +4,7 @@ from django.template import loader
 
 def index(request):
     template=loader.get_template('tracker_fe/index.html')
-    context={'message':"Hello, world. You are at the tracking system"}
+    context={'message':"Hello, world. You are at the tracking system now"}
     return render(request,'tracker_fe/index.html',context)
 
     
