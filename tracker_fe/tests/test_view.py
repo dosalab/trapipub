7397fee63afb8b47f7720a5fbe88b227206dcf9d
Aboobacker <aboobacker@hamon.in>
2016-12-22 10:34:52 +0530

    Fixes tests for python3

--- tracker_fe/tests/test_view.py
+++ tracker_fe/tests/test_view.py
@@ -4,4 +4,4 @@ from ..views import index
 
 def test_index_view(client):
     response = client.get(reverse('tracker_fe:index'))
-    assert "Hello, world. You are at the tracking system" in response.content
+    assert "Hello, world. You are at the tracking system" in response.content.decode('utf-8')
