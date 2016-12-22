e856ee8efa0ecc7384d06efe53b3108e6705baa6
Aboobacker <aboobacker@hamon.in>
2016-12-22 22:38:23 +0530

    add urls for drf application tracker api

---
+++ tracker_api/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import url,include
+from rest_framework import routers
+from tracker_api import views
+
+router = routers.DefaultRouter()
+
+app_name='tracker_api'
+
+urlpatterns = [
+    url(r'^log/$',views.logView.as_view({'get':'list'}), name='login'),
+    url(r'^carrier/$',views.carrierView.as_view({'get':'list','post':'create'}),name='carrier'),
+    ]
+
