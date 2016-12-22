4f0fd2f2170d9888d0d68efbf9f4378d34bb437a
Aboobacker <aboobacker@hamon.in>
2016-12-22 10:32:11 +0530

    Updates bootstrap app name

--- tracker/settings.py
+++ tracker/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bootstrap_toolkit',
+    'bootstrap3'
 ]
 
 MIDDLEWARE = [
