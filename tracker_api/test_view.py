ec76a80e3ef28e47782967ba7358f721e3987df3
Aboobacker <aboobacker@hamon.in>
2017-01-11 17:17:49 +0530

    Add tests for carrier creation by merchant,list all carriers of a mercahnt , list details of a carrier , cahnge details of a carrier

--- tracker_api/test_view.py
+++ tracker_api/test_view.py
@@ -4,8 +4,8 @@ from django.contrib.auth.models import User
 from .views import carrierView
 from rest_framework.authtoken.models import Token
 from rest_framework.test import APIClient
-
-
+import json
+from .models import Merchant,Carrier
 @pytest.mark.django_db
 def test_carrier_view_with_no_merchant(client):
     user=User.objects.create_user("user","useraddress", "aaasssddd")
@@ -14,4 +14,77 @@ def test_carrier_view_with_no_merchant(client):
     client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
     response = client.post(reverse('tracker_api:carrier'),{'name':"carrer",'phone':"99798798",'location':"here",'user':user,'merchant':"null"})
     assert response.status_code == 403
+
+@pytest.mark.django_db
+def test_carrier_view_by_merchant(client):
+    merchant=client.post(reverse('registration_register'),{"username":"newuser","email":"user@gmail.com","password1":"aaasssddd","password2":'aaasssddd',"name":"merchant1",'address':"merchantaddress"})
+    merchant=User.objects.get(username="newuser").merchant
+    token = Token.objects.get(user__username='newuser')
+    client = APIClient()
+    client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
+    response = client.post(reverse('tracker_api:carrier'),{'name':"carrer",'phone':"99798798",'location':"here","username":"carrieruser","password":"aaasssddd","email":"carrier@gmail.com"})
+    assert response.status_code == 201
+
+
+@pytest.mark.django_db
+def test_all_carriers_of_merchant(client):
+    user=User.objects.create_user("user","user@tracker.com", "aaasssddd")
+    Merchant.objects.create(name="merchant1",address="merchantaddress",user=user)
+    merchant=User.objects.get(username="user").merchant
+    token = Token.objects.get(user__username='user')
+    client = APIClient()
+    client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
+
+    cu1=User.objects.create_user("carrier1","user@tracker.com", "aaasssddd")
+    Carrier.objects.create(name="carrer1",phone="9656888871",location="kozikkod",merchant=merchant,user=cu1)
     
+    cu2=User.objects.create_user("carrier2","user@tracker.com", "aaasssddd")
+    Carrier.objects.create(name="carrer2",phone="99999999",location="palakkad",merchant=merchant,user=cu2)
+
+    cu3=User.objects.create_user("carrier3","user@tracker.com", "aaasssddd")
+    Carrier.objects.create(name="carrer3",phone="99999999",location="kerala",merchant=merchant,user=cu3)
+
+    response = client.get(reverse('tracker_api:get_carriers'))
+    op= [{"url": "http://testserver/api/v1/carrier/1"},{ "url": "http://testserver/api/v1/carrier/2"},{ "url": "http://testserver/api/v1/carrier/3"}]
+    assert response.data == op
+    
+
+
+
+@pytest.mark.django_db
+def test_details_of_carrier(client):
+    user=User.objects.create_user("user","user@tracker.com", "aaasssddd")
+    Merchant.objects.create(name="merchant1",address="merchantaddress",user=user)
+    merchant=User.objects.get(username="user").merchant
+    token = Token.objects.get(user__username='user')
+    client = APIClient()
+    client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
+
+    cu1=User.objects.create_user("carrier1","user@tracker.com", "aaasssddd")
+    Carrier.objects.create(name="carrier",phone="9656888871",location="kozikkod",merchant=merchant,user=cu1)
+   
+    response = client.get(reverse('tracker_api:carrierdetail',args=[1]))
+    op = {"id":1,"location":"kozikkod","name":"carrier","phone":"9656888871"}
+    assert response.data == op
+
+
+@pytest.mark.django_db
+def test_change_details_of_carrier(client):
+    user=User.objects.create_user("user","user@tracker.com", "aaasssddd")
+    Merchant.objects.create(name="merchant1",address="merchantaddress",user=user)
+    merchant=User.objects.get(username="user").merchant
+    token = Token.objects.get(user__username='user')
+    client = APIClient()
+    client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
+
+    cu1=User.objects.create_user("carrier1","user@tracker.com", "aaasssddd")
+    Carrier.objects.create(name="carrier",phone="9656888871",location="kozikkod",merchant=merchant,user=cu1)
+
+    response = client.get(reverse('tracker_api:carrierdetail',args=[1]))
+    assert response.data ==  {"id":1,"location":"kozikkod","name":"carrier","phone":"9656888871"}
+
+    client.patch(reverse('tracker_api:carrierdetail',args=[1]),{"name":"new name"})
+    response = client.get(reverse('tracker_api:carrierdetail',args=[1]))
+    assert response.data ==  {"id":1,"location":"kozikkod","name":"new name","phone":"9656888871"}
+
+   
