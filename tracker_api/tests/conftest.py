fbdf77a94dc7722ef4dbce4517f3b7f300d9a5fd
Aboobacker <aboobacker@hamon.in>
2017-01-17 12:59:08 +0530

    integration test for test_api application

---
+++ tracker_api/tests/conftest.py
@@ -0,0 +1,23 @@
+import pytest
+from django.conf import settings
+
+@pytest.fixture(scope='session')
+def django_db_setup():
+    settings.DATABASES['default'] = {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'testdb',
+        'USER': 'abu',
+        'PASSWORD': 'trackerpwd',
+        }
+
+@pytest.fixture
+def server(xprocess):
+    def preparefunc(cwd):
+        print (str(cwd))
+        n = cwd.new(basename="").new(basename="").dirpath().chdir()
+        print ("Working directory is {}".format(n))
+        path = cwd.join("../../manage.py").strpath
+        print(path)
+        return (r".*Quit the server with CONTROL-C.*", ["python", path, "runserver", "8000"])
+    
+    logfile = xprocess.ensure("tracker-server", preparefunc)
---
+++ tracker_api/tests/test_integ.py
@@ -0,0 +1,44 @@
+import pytest
+import requests
+import sys
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
+
+@pytest.mark.django_db
+def test_merchant(server):
+    URL = "http://127.0.0.1:8000/accounts/register/"
+    client = requests.session()
+    client.get(URL)
+    csrftoken = client.cookies['csrftoken']
+    login_data = dict(username="newuser1234",email="newuser@tracker.com",password1="aaasssddd",password2="aaasssddd",name="newmerchant",address="merchantaddress", csrfmiddlewaretoken=csrftoken)
+    r = client.post(URL, data=login_data, headers=dict(Referer=URL))
+    
+    assert  User.objects.get(username="newuser1234").merchant.name == "newmerchant"
+    
+    token1 = client.post('http://127.0.0.1:8000/token/',{'username':"newuser1234",'password':"aaasssddd"})
+    token2 = Token.objects.get(user__username='newuser1234').key
+    token1=token1.json()['token']
+
+    assert token1 == token2
+    
+    carrier=client.post('http://127.0.0.1:8000/api/v1/carrier/',{'name':"newcarrier",'phone':"99999",'location':"here",'username':"carriernewuser",'password':"aaasssddd",'email':"carrier@trcker.com"},headers={'Authorization':'Token '+token1})
+    import pdb;pdb.set_trace()
+ 
+    
+    
+# @pytest.mark.django_db
+# def test_carrier_creation(server):
+#     URL = "http://127.0.0.1:8000/accounts/register/"
+#     client = requests.session()
+#     client.get(URL)
+#     csrftoken = client.cookies['csrftoken']
+#     login_data = dict(username="newuser1234",email="newuser@tracker.com",password1="aaasssddd",password2="aaasssddd",name="newmerchant",address="merchantaddress", csrfmiddlewaretoken=csrftoken)
+#     r = client.post(URL, data=login_data, headers=dict(Referer=URL))
+    
+#     tok = client.post('http://127.0.0.1:8000/token/',{'username':"newuser1234",'password':"aaasssddd"})
+#     token = Token.objects.get(user__username='newuser')
+#     client = APIClient()
+#     client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
+#     carrier=client.post('http://127.0.0.1:8000/api/v1/carrier/',{'name':"newcarrier",'phone':"99999",'location':"here",'username':"carriernewuser",'password':"aaasssddd",'email':"carrier@trcker.com"})
+#     import pdb;pdb.set_trace()
+    
