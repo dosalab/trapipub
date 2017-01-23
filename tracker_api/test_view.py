83cf3e955c007a28bdd19bd8121bda253d5d55ef
Aboobacker <aboobacker@hamon.in>
2017-01-23 11:07:21 +0530

    test for check the order creation with bad carrier

--- tracker_api/test_view.py
+++ tracker_api/test_view.py
@@ -246,4 +246,11 @@ def test_order_creat_bad_customer(merchant_client, order_data):
                                     order_data)
 
     assert response.status_code == 400
+
+@pytest.mark.django_db
+def test_order_creat_bad_amount(merchant_client, order_data):
+    del order_data["amount"]
+    response = merchant_client.post(reverse('tracker_api:orders'),
+                                    order_data)
+    assert response.status_code == 400
     assert response.status_code == 201
