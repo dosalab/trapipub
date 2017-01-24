d09fd5967a5b29c08fdf6931e35039ef8dd94cc2
Aboobacker <aboobacker@hamon.in>
2017-01-24 10:29:20 +0530

    Add validator in phone field

--- tracker_api/migrations/0001_initial.py
+++ tracker_api/migrations/0001_initial.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 
 import datetime
 from django.conf import settings
+import django.core.validators
 from django.db import migrations, models
 import django.db.models.deletion
 
@@ -32,8 +33,7 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('name', models.CharField(max_length=50)),
                 ('address', models.CharField(max_length=30)),
-                ('phone', models.IntegerField()),
-                ('user', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
+                ('phone', models.CharField(max_length=15, validators=[django.core.validators.RegexValidator(message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.", regex='^\\+?1?\\d{9,15}$')])),
             ],
         ),
         migrations.CreateModel(
