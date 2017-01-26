1e0c7c7c14f7ae202cba2b5b354f48ff4555e800
Aboobacker <aboobacker@hamon.in>
2017-01-26 14:01:38 +0530

    Add new allowed VPS

--- tracker/settings/base.py
+++ tracker/settings/base.py
@@ -19,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','ec2-54-64-56-135.ap-northeast-1.compute.amazonaws.com']
 
 # Application definition
 
