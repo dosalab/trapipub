47194ce2e48475c40f136625b30dc76cc5ed7066
Aboobacker <aboobacker@hamon.in>
2017-01-27 16:44:40 +0530

    Pylint

--- tracker_api/serializer.py
+++ tracker_api/serializer.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import User
-from .models import Carrier,Order,Merchant,Customer,Delivery,Status
+from .models import Carrier, Order, Merchant, Customer, Delivery, Status
 from rest_framework import serializers
 from django.db import transaction
 from django.utils.text import slugify
@@ -10,20 +10,20 @@ from django.contrib.sites.shortcuts import get_current_site
 class MerchantSerializer(serializers.ModelSerializer):
     class Meta:
         model = Merchant
-        fields = ('name', 'address', 'payment_info','user')
+        fields = ('name', 'address', 'payment_info', 'user')
 
 #Carrier creation 
 class CarrierSerializer(serializers.Serializer):
-    name = serializers.CharField(required = True)
-    phone = serializers.IntegerField(required = True)
-    username = serializers.CharField(required = True)
-    password = serializers.CharField(required = True)
-    email = serializers.EmailField(required = True)
-    location = serializers.CharField(required = False,default="")
+    name = serializers.CharField(required=True)
+    phone = serializers.IntegerField(required=True)
+    username = serializers.CharField(required=True)
+    password = serializers.CharField(required=True)
+    email = serializers.EmailField(required=True)
+    location = serializers.CharField(required=False, default="")
             
     def create(self, validated_data):
         merchant = self.context['merchant']
-        name  = validated_data['name']
+        name = validated_data['name']
         phone = validated_data['phone']
         username = validated_data['username']
         password = validated_data['password']
@@ -31,11 +31,11 @@ class CarrierSerializer(serializers.Serializer):
         location = validated_data['location']
         with transaction.atomic():
             u = User.objects.create_user(username, email, password)
-            c = Carrier(name = name,
-                        phone = phone,
-                        location = location,
-                        merchant = merchant,
-                        user = u)
+            c = Carrier(name=name,
+                        phone=phone,
+                        location=location,
+                        merchant=merchant,
+                        user=u)
             c.slug = slugify(u.username+merchant.name)
             c.save()
             return c
@@ -50,15 +50,16 @@ class GetCarrierSerializer(serializers.ModelSerializer):
 # Update details of a carrier
 class PatchCarrier(serializers.ModelSerializer):
     class Meta:
-       model = Carrier
-       fields =("name","phone","location")
+        model = Carrier
+        fields = ("name", "phone", "location")
 
 class CustomHyperlink(serializers.HyperlinkedIdentityField):
     def get_url(self, obj, view_name, request, format):
         url_kwargs = {
             'slug': obj.slug,
         }
-        return 'http://{}{}'.format(get_current_site(request),reverse(view_name, kwargs=url_kwargs))
+        return 'http://{}{}'.format(get_current_site(request),
+                                    reverse(view_name, kwargs=url_kwargs))
 
 #Get all carriers urls under a merchant
 class CarrierUrlSerializer(serializers.HyperlinkedModelSerializer):
@@ -68,20 +69,20 @@ class CarrierUrlSerializer(serializers.HyperlinkedModelSerializer):
     )
     class Meta:
         model = Carrier
-        fields =("url",)
+        fields = ("url",)
 
 # create a customer
 class CustomerSerializer(serializers.Serializer):
-    name = serializers.CharField(required = True)
-    phone = serializers.IntegerField(required = True)
-    username = serializers.CharField(required = True)
-    password = serializers.CharField(required = True)
-    email = serializers.EmailField(required = True)
-    address = serializers.CharField(required = True)
+    name = serializers.CharField(required=True)
+    phone = serializers.IntegerField(required=True)
+    username = serializers.CharField(required=True)
+    password = serializers.CharField(required=True)
+    email = serializers.EmailField(required=True)
+    address = serializers.CharField(required=True)
    
     def create(self, validated_data):
         merchant = self.context['merchant']
-        name  = validated_data['name']
+        name = validated_data['name']
         phone = validated_data['phone']
         username = validated_data['username']
         password = validated_data['password']
@@ -89,16 +90,15 @@ class CustomerSerializer(serializers.Serializer):
         address = validated_data['address']
         with transaction.atomic():
             usr = User.objects.create_user(username, email, password)
-            cus = Customer(name = name,
-                        phone = phone,
-                        address = address,
-                        merchant = merchant,
-                         user = usr)
+            cus = Customer(name=name,
+                           phone=phone,
+                           address=address,
+                           merchant=merchant,
+                           user=usr)
             cus.slug = slugify(usr.username+merchant.name)
             cus.save()
             return cus
         
-      
 # Get all orders under a carrier
 class CustomerUrlSerializer(serializers.ModelSerializer):
     url = serializers.HyperlinkedIdentityField(
@@ -107,36 +107,35 @@ class CustomerUrlSerializer(serializers.ModelSerializer):
     )
     class Meta:
         model = Customer
-        fields =('url',)
+        fields = ('url',)
 
 # Get details of a customer
 class CustomerDetailsSerializer(serializers.ModelSerializer):
     class Meta:
-       model = Customer
-       fields =('name','phone','address')
+        model = Customer
+        fields = ('name', 'phone', 'address')
 
 # Create an order
 class OrderSerializer(serializers.Serializer):
-    customer = serializers.CharField(required = True)
-    notes = serializers.CharField(required = True)
-    amount = serializers.IntegerField(required = True)
-    invoice_number = serializers.CharField(required = True)
+    customer = serializers.CharField(required=True)
+    notes = serializers.CharField(required=True)
+    amount = serializers.IntegerField(required=True)
+    invoice_number = serializers.CharField(required=True)
    
     def create(self, validated_data):
         merchant = self.context['merchant']
-        customer  = self.context['customer']
+        customer = self.context['customer']
         notes = validated_data['notes']
         amount = validated_data['amount']
         invoice_number = validated_data['invoice_number']
         date = datetime.datetime.now()
         with transaction.atomic():
-            order = Order(merchant = merchant,
-                          customer=customer ,
+            order = Order(merchant=merchant,
+                          customer=customer,
                           notes=notes,
                           invoice_number=invoice_number,
                           amount=amount,
-                          date=date
-                      )
+                          date=date)
             order.slug = slugify(invoice_number)
             order.save()
             return order
@@ -145,38 +144,36 @@ class OrderSerializer(serializers.Serializer):
 class OrderUrlSerializer(serializers.ModelSerializer):
     url = serializers.HyperlinkedIdentityField(
         view_name='tracker_api:orderdetail',
-        lookup_field='slug'
-    )
+        lookup_field='slug')
     class Meta:
         model = Order
-        fields =('url',)
+        fields = ('url',)
 
 #get detaisl of a order
 class orderdetailsSerializer(serializers.ModelSerializer):
     class Meta:
-       model = Order
-       fields =('invoice_number','date','amount','customer','notes')
+        model = Order
+        fields = ('invoice_number', 'date', 'amount', 'customer', 'notes')
 
 
 # Create a delivery
 class DeliverySerializer(serializers.Serializer):
-    order = serializers.CharField(required = True)
-    carrier = serializers.CharField(required = True)
+    order = serializers.CharField(required=True)
+    carrier = serializers.CharField(required=True)
     def create(self, validated_data):
         order = self.context['order']
-        carrier  = self.context['carrier']
-        delivery = Delivery(order = order,
-                            carrier=carrier )
+        carrier = self.context['carrier']
+        delivery = Delivery(order=order,
+                            carrier=carrier)
         delivery.slug = slugify(order.slug+carrier.name)
         with transaction.atomic():
             delivery.save()
             delivery = Delivery.objects.get(slug=delivery.slug)
-            date = datetime.datetime.now() 
+            date = datetime.datetime.now()
             st = Status(delivery=delivery,
-                        date =  date,
-                        info = "Get ready",
-                        terminal = False
-                    )
+                        date=date,
+                        info="Get ready",
+                        terminal=False)
             st.save()
         return delivery
 
@@ -189,8 +186,10 @@ class StatusSerializer(serializers.ModelSerializer):
 
 #custom related field for status
 class StatusRelatedField(serializers.RelatedField):
-     def to_representation(self, value):
-          return {"date":value.date,"info":value.info,"terminal":value.terminal}
+    def to_representation(self, value):
+        return {"date":value.date,
+                "info":value.info,
+                "terminal":value.terminal}
         
 # get delivery details
 class DeliveryDetailsSerializer(serializers.ModelSerializer):
