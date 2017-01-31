617f4215fd20597f25549849e470b1b8b48b7607
Aboobacker <aboobacker@hamon.in>
2017-01-31 13:15:18 +0530

    fix not run server

--- tracker_fe/tests/conftest.py
+++ tracker_fe/tests/conftest.py
@@ -1,11 +1,11 @@
 import pytest
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def server(xprocess):
     def preparefunc(cwd):
         print (str(cwd))
         n = cwd.new(basename="").new(basename="").dirpath().chdir()
         print ("Working directory is {}".format(n))
-        return ("Quit the server with CONTROL-C", ["python", "../../manage.py", "runserver", "9000"])
+        return ("Quit the server with CONTROL-C", ["python", "../../manage.py", "runserver", "8000"])
     
     logfile = xprocess.ensure("tracker-server", preparefunc)
