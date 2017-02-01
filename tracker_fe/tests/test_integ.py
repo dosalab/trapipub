b9070457e1f86ee262a5f8b31aef54ee8369844e
Aboobacker <aboobacker@hamon.in>
2017-02-01 16:56:22 +0530

    fix port error

--- tracker_fe/tests/test_integ.py
+++ tracker_fe/tests/test_integ.py
@@ -1,7 +1,7 @@
 import requests
 
 def test_index_page(server):
-    resp = requests.get("http://127.0.0.1:9000/")
+    resp = requests.get("http://127.0.0.1:8000/")
     assert "Hello, world. You are at the tracking system" in resp.content.decode('utf-8')
     
 
