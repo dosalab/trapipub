2cd57d1c2424e27cb48aac0e4d13423f5b82550b
Aboobacker <aboobacker@hamon.in>
2017-02-07 13:39:50 +0530

    Change address field into point field

--- tracker_api/models.py
+++ tracker_api/models.py
@@ -53,7 +53,7 @@ class Customer (models.Model):
     phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
     name = models.CharField(max_length=50)
     phone = models.CharField(validators=[phone_regex],max_length=15)
-    address = models.CharField(max_length=20)
+    address = gmodels.PointField(null=True)
     user = models.OneToOneField(User, on_delete=models.CASCADE,null=True)
     slug = models.SlugField(max_length=50, default='slug', primary_key=True )
 
