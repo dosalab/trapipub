2e55251d4939aaac72aa7d74463a4085f22c8dd3
Aboobacker <aboobacker@hamon.in>
2017-02-12 19:29:20 +0530

    Test for customfield

---
+++ tracker_api/tests/test_fields.py
@@ -0,0 +1,23 @@
+import json
+from pytest import raises
+from rest_framework.serializers import ValidationError
+from tracker_api.customfields import ForwardField
+
+def test_bad_name(request):
+    with raises(ValidationError) as exc:
+        ForwardField.to_representation(request, "kozikkod")
+    assert "Invalid address" in str(exc.value)
+
+# def test_repeated_names(request):
+#     with raises(ValidationError) as exc:
+#         ForwardField.to_representation(request, "safsaf")
+#     assert "Have more points in this address" in str(exc.value)
+
+def test_without_name(request):
+    with raises(ValidationError) as exc:
+        ForwardField.to_representation(request, "")
+    assert "Invalid address" in str(exc.value)
+
+def test_without_name(request):
+    response = ForwardField.to_representation(request, "Calicut railway station")
+    assert json.loads(response.geojson) == {"type": "Point", "coordinates": [75.781116, 11.24621]}
